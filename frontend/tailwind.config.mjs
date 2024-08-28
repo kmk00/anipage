37d4fa6d1ffdd758f2dfe8738295a1628f80163c
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-08-28 16:19:45 +0200

    Initialize a new project

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      primary: '#1B1B1A',
+      secondary: '#F8F6F8',
+      accent: '#72EDF8',
+      primaryLight: '#292928'
+    },
+    extend: {}
+  },
+  plugins: []
+}
