d38e14bc048d8b91ad9ded7f5ce0fa88bea90e7a
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-08-30 12:08:39 +0200

    Better letters filtering

--- frontend/src/components/Navigation/LetterSelect.tsx
+++ frontend/src/components/Navigation/LetterSelect.tsx
@@ -1,12 +1,16 @@
-const LetterSelect = () => {
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ#'.split('')
+type LetterSelectProps = {
+  alphabetFiltered?: string[]
+}
+
+const LetterSelect = ({ alphabetFiltered }: LetterSelectProps = {}) => {
+  const alphabet = alphabetFiltered || '#ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
   return (
     <div className="py-2 grid grid-cols-11 text-center text-secondary ">
       {alphabet.map((letter, index) => (
         <a
           className={`${
-            index % 2 === 0 ? 'bg-primary' : 'bg-primaryLight'
+            index % 2 === 0 ? 'bg-primaryLight' : 'bg-primary'
           } hover:bg-accent hover:text-primary transition-all duration-120 ease-in cursor-pointer `}
           href={`#studios-${letter}`}
           key={letter}
--- frontend/src/components/Navigation/StudioList.tsx
+++ frontend/src/components/Navigation/StudioList.tsx
@@ -7,16 +7,6 @@ type StudioListProps = {
 }
 
 const StudioList = ({ studioList, search }: StudioListProps) => {
-  const filteredStudios = Object.keys(studioList).reduce((acc, letter) => {
-    const studios = studioList[letter].filter((studio) =>
-      studio.toLowerCase().includes(search.toLowerCase())
-    )
-    if (studios.length > 0) {
-      acc[letter] = studios
-    }
-    return acc
-  }, {} as StudioList)
-
   if (search.trim() === '') {
     return (
       <div>
@@ -30,6 +20,18 @@ const StudioList = ({ studioList, search }: StudioListProps) => {
     )
   }
 
+  const filteredStudios = Object.keys(studioList).reduce((acc, letter) => {
+    const studios = studioList[letter].filter((studio) =>
+      studio.toLowerCase().includes(search.toLowerCase())
+    )
+    if (studios.length > 0) {
+      acc[letter] = studios
+    }
+    return acc
+  }, {} as StudioList)
+
+  const filteredAlphabetString = Object.keys(filteredStudios).join('').split('')
+
   return (
     <div>
       <div className=" max-h-[70vh] overflow-y-auto scroll-smooth">
@@ -41,7 +43,7 @@ const StudioList = ({ studioList, search }: StudioListProps) => {
           />
         ))}
       </div>
-      <LetterSelect />
+      <LetterSelect alphabetFiltered={filteredAlphabetString} />
     </div>
   )
 }
